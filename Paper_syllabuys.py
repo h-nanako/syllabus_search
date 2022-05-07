bf9708bbb63fad4e2c757702fd999db210451eac
Nanako <87801494+h-nanako@users.noreply.github.com>
2022-05-07 16:58:35 +0900

    Update Paper_syllabuys.py

--- Paper_syllabuys.py
+++ Paper_syllabuys.py
@@ -6,7 +6,7 @@ import MeCab
 def vector(syllabus_vector2):
     syllabus_vector1 = np.load('vector.npy')
     # ベクトルの類似度を計算
-    u_answer = [1 - spatial.distance.cosine(syllabus_vector1[i], syllabus_vector2) for i in range(88)] # コサイン類似度
+    u_answer = [1 - spatial.distance.cosine(syllabus_vector1[i], syllabus_vector2) for i in range(len(syllabus_vector1))] # コサイン類似度
 
     # 距離が近い科目を上位10個出力
     with open('syllabus_title.txt', 'r', encoding='utf-8')as f:
